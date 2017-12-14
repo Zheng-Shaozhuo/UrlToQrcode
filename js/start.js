402ce7b23df8675c82e741267e9de006341e6d92
Shaozhuo <Zheng-Shaozhuo@outlook.com>
2017-12-14 21:06:31 +0800

    Url链接转二维码v1.0

---
+++ js/start.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+	chrome.tabs.getSelected(function(tab) {
+		var url = 'http://pan.baidu.com/share/qrcode?w=320&h=320&url=' + tab.url; 
+		document.getElementById('a').innerHTML = tab.title;
+		document.getElementById('a').style.title = tab.title;
+		document.getElementById('img').src = url;
+	});
+});
