4b1b2b8c5b6cd665567cb38be00a42a929d90f7e
ZhengShaozhuo <Zheng-Shaozhuo@outlook.com>
2017-12-14 22:52:12 +0800

    v2.0版本

--- js/start.js
+++ js/start.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	chrome.tabs.getSelected(function(tab) {
 		var url = 'http://pan.baidu.com/share/qrcode?w=320&h=320&url=' + tab.url; 
 		document.getElementById('a').innerHTML = tab.title;
-		document.getElementById('a').style.title = tab.title;
+		document.getElementById('a').title = tab.title;
 		document.getElementById('img').src = url;
 	});
 });
